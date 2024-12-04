5e48e98a3e7d615b6caa0a54cecc20cdae4930c6
Kisang Hwang <hgd4859@gmail.com>
2024-12-03 20:47:07 -0800

    set up redux global variable

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,4 +1,5 @@
-import '@/app/ui/global.css';
+import "@/app/ui/global.css";
+import StoreProvider from "./provider";
 
 export default function RootLayout({
   children,
@@ -7,7 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <StoreProvider>{children}</StoreProvider>
+      </body>
     </html>
   );
 }
