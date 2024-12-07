b898fc42adab72ac1105004127e2ffc374ecdfb4
Kisang Hwang <hgd4859@gmail.com>
2024-12-06 23:23:13 -0800

    fix_signout2

--- nextjs-dashboard/app/dashboard/events/page.tsx
+++ nextjs-dashboard/app/dashboard/events/page.tsx
@@ -1,10 +1,19 @@
-import AcmeLogo from "@/app/ui/ticketpro-logo";
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
-import Link from "next/link";
+"use client";
 import EventList from "./event_list";
 import TicketProLogo from "../../ui/ticketpro-logo";
+import { useSelector } from "react-redux";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Page() {
+  const login = useSelector((state: any) => state.currentLogin.value);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (login.id == "") {
+      router.push("/");
+    }
+  }, [login, router]);
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-45">
